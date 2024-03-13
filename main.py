00ddf1ad48637cd2e058f871d1ab9b088de4b5f0
RJW <blacksheeprobotics@gmail.com>
2024-03-12 23:28:32 -0400

    added entry boxes; added calculated conversion

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ window.minsize(width=300, height=200)
 
 
 # Calculate Button
-def button_calculate(miles=1.0):
-    print(round(miles * 0.621371))
+def button_calculate():
+    miles = float(input_miles.get())
+    kilometers = miles * 1.609344
+    print(f"Miles: {miles}\nKilometers: {kilometers}")
+    label_conversion.config(text=round(kilometers))
 
 
+# Button-Calculate
 button = tk.Button(text="Calculate", command=button_calculate)
 button.grid(row=3, column=2)
+
+# Text - is equal to
+label_convert = tk.Label(text="is equal to")
+label_convert.grid(row=2, column=1)
+# Text - Miles
+label_miles = tk.Label(text="Miles")
+label_miles.grid(row=1, column=3)
+# Text - Answer
+label_conversion = tk.Label()
+label_conversion.grid(row=2, column=2)
+# Text - Km
+label_kilometers = tk.Label(text="Km")
+label_kilometers.grid(row=2, column=3)
+
+# Entry-Box
+input_miles = tk.Entry(width=10)
+input_miles.grid(row=1, column=2)
 
 window.mainloop()
