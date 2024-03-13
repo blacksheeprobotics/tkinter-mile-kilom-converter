50877cca2092006409a474a3ce9bc6403719adf6
RJW <blacksheeprobotics@gmail.com>
2024-03-12 23:16:54 -0400

    added calculate button

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ window = tk.Tk()
 window.title("Mile to Km Converter")
 window.minsize(width=300, height=200)
 
+
+# Calculate Button
+def button_calculate(miles=1.0):
+    print(round(miles * 0.621371))
+
+
+button = tk.Button(text="Calculate", command=button_calculate)
+button.grid(row=3, column=2)
+
 window.mainloop()
